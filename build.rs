7a0eb6fb1dc84790a23b1fa4e3ed4b7be3b77159
HBchevelle68 <aportante10@gmail.com>
2023-02-11 14:37:49 -0500

    Added no debug and stripped version

--- build.rs
+++ build.rs
@@ -9,6 +9,12 @@ fn main() {
         .status()
         .unwrap();
 
+    // Build exmaple without debug and symbols stripped
+    Command::new("gcc")
+        .args(["example/test.c", "-s", "-o", "example/test_no_dbg_stripped"])
+        .status()
+        .unwrap();
+
     // Build example with debug
     Command::new("gcc")
         .args(["example/test.c", "-g", "-o", "example/test_dbg"])
