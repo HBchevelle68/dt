776499d7f4a782728d966cdb9c9481763f65810c
HBchevelle68 <aportante10@gmail.com>
2023-02-18 17:15:02 -0500

    Restructured to return a terminal error code

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,49 @@
-//use clap::Parser;
-//use goblin::elf::Elf;
 use std::{fs, path::PathBuf};
 
 mod cli;
 mod disasm;
-mod dt_elf;
+mod dtelf;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+mod codes;
+use codes::DtCode;
+
+fn dt() -> DtCode {
     let matches = cli::dt_cli().get_matches();
 
     let arg_path = matches.get_one::<PathBuf>("FILE").unwrap();
-    let bytes: Vec<u8> = fs::read(arg_path)?;
-
-    // Figure out what we need to do next
-    if let Some(arg_func) = matches.get_one::<String>("func") {
-        return match dt_elf::has_debug(&bytes) {
-            Ok(true) => {
-                println!("[+] Debug symbols found");
-                disasm::src_disasm(arg_path, arg_func)
+    let res = match fs::read(arg_path) {
+        Ok(bytes) => {
+            let mut result = DtCode::Success;
+            if let Some(arg_func) = matches.get_one::<String>("func") {
+                // Disassemble a targeted function
+                match disasm::has_debug(&bytes) {
+                    Ok(true) => {
+                        println!("[+] Debug symbols found");
+                        disasm::src_disasm(arg_path, arg_func);
+                    }
+                    Ok(false) => {
+                        println!("[*] No debug symbols found");
+                        disasm::no_src_disasm(arg_path, arg_func);
+                    }
+                    Err(e) => {
+                        println!("{e}");
+                        result = DtCode::ElfParse;
+                    }
+                };
+            } else if let Some(_lsym) = matches.get_one::<bool>("list") {
+                println!("wee");
             }
-            Ok(false) => {
-                println!("[*] No debug symbols found");
-                disasm::no_src_disasm(arg_path, arg_func)
-            }
-            Err(e) => Err(e),
-        };
-    } else if let Some(_lsym) = matches.get_one::<bool>("list") {
-        return dt_elf::get_all_syms(&bytes);
-    }
+            result
+        }
+        Err(e) => {
+            println!("{e}");
+            DtCode::IoReadFail
+        }
+    };
 
-    Ok(())
+    res
+}
+
+fn main() {
+    std::process::exit(dt() as i32);
 }
